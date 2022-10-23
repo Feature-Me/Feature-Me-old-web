a8c81d65429ddb911c234c1a6447d56f8b61fa74
Mksk <sakasakamakki@outlook.jp>
2022-10-23 21:08:49 +0900

    started to make rework background

--- src/promotions/webpack.config.js
+++ src/promotions/webpack.config.js
@@ -7,20 +7,12 @@ module.exports = {
         filename: "bundle.js"
     },
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        extensions: ['.js', '.jsx'],
         plugins: [new TsconfigPathsPlugin({ configFile: path.join(__dirname,"../","tsconfig.json") })]
     },
     target: "web",
     module: {
         rules: [
-            { test: /\.tsx?$/, loader: 'ts-loader' },
-            { test: /\.ts?$/, loader: 'ts-loader' },
-            { test: /\.jsx?$/, loader: 'babel-loader' },
-            { test: /\.(scss|css)$/, use: ['style-loader', { loader: 'css-loader', options: { modules: true } }, 'postcss-loader', 'sass-loader'] },
-            { test: /\.(jpg|png|ico)$/, type: "asset/inline" },
-            { test: /\.(ttf|TTF|otf|OTF)$/, /* loader: 'url-loader', */type: "asset/inline",/* generator: {dataUrl:context=> "data:font/otf;base64,"+context.toString("base64")} */ },
-            { test: /\.cur?$/, type: "asset/inline", generator: { dataUrl: context => "data:image/cur;base64," + context.toString("base64") } },
-            { test: /\.svg$/, type: "asset/inline" },
         ]
     }
 }
