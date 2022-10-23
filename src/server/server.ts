6225f48fb6d1c1b3b722c6ccf504ef34a7ad04f7
Mksk <sakasakamakki@outlook.jp>
2022-10-23 16:45:04 +0900

    added server healthcheck

--- src/server/server.ts
+++ src/server/server.ts
@@ -5,15 +5,10 @@ import * as socketIo from "socket.io";
 import {instrument} from "@socket.io/admin-ui"
 import * as fs from "fs";
 import * as crypto from "crypto";
-//import * as nedb from "nedb";
 import * as url from "url";
 import * as dotenv from "dotenv"
 import {v4 as uuidv4, v5 as uuidv5} from "uuid";
-/* import * as discord from "discord.js"
-import { commandModules } from "./command"; */
-
-//if (process.env.NODE_ENV != "production") dotenv.config({ path: path.join(__dirname, "../../../.env") });
-
+import * as os from "os";
 
 //express server
 const app = express();
@@ -26,11 +21,6 @@ const scriptsdir: string = path.join(basedir, "scripts");
 const imagedir: string = path.join(basedir, "images");
 const dbdir: string = path.join(basedir, "db");
 const port: number = Number(process.env.PORT) || 3000;
-
-/* const db = {
-    users: new nedb({ filename: path.join(dbdir, "users.db"), autoload: true }),
-    leaderboard: new nedb({ filename: path.join(dbdir, "leaderboard.db"), autoload: true })
-} */
 
 //web socket
 const io = new socketIo.Server(server,{
@@ -138,64 +128,20 @@ app.get("/update/map", (req, res) => {
 })
 
 
+app.get("/health",(req,res)=>{
+    res.status(200).end("Server online.");
+    console.log({
+        memory:{
+            used: process.memoryUsage().heapUsed,
+            all:os.totalmem(),
+            free:os.freemem()
+        },
+        upTime:process.uptime(),
+        usedCpu:process.cpuUsage()
+    });
+    
+})
+
 app.use((req, res, next) => {
     res.status(404).redirect("/");
 });
-
-
-
-
-/* //discord
-const client = new discord.Client({ intents: [discord.GatewayIntentBits.Guilds, discord.GatewayIntentBits.DirectMessages] });
-const rest = new discord.REST({ version: '10' }).setToken(process.env.DISCORD_BOT_TOKEN || "");
-
-const commandFiles = fs.readdirSync(path.join(__dirname, "./commands")).filter(file => file.endsWith('.js'));
-let commands = new discord.Collection<string, commandModules>();
-for (const file of commandFiles) {
-    const command: { default: commandModules } = require(`./commands/${file}`);
-
-    commands.set(command.default.command.name, command.default);
-}
-
-async function register() {
-    if (!client.user?.id) return;
-    const commandData = commands.map(cmd => cmd.command)
-    const data = await rest.put(discord.Routes.applicationCommands(client.user.id), { body: commandData })
-    return client.application!.commands.set(commandData);
-}
-
-client.on("ready", async (e) => {
-    console.log(`Logged in as ${e.user.tag}`);
-    try {
-        await register();
-    } catch (error) {
-        console.error(error);
-    }
-
-    client.users.fetch(process.env.OWNER_ID || "").then(user => {
-        if (!user) throw Error("user not found");
-        //user.send(`logged in to discord.\nStarted:${new Date().toString()}`);
-        const embed = new discord.EmbedBuilder()
-            .setColor(0x1189da)
-            .setTitle("Feature Me Server Started.")
-            .setTimestamp()
-        user.send({ embeds: [embed] })
-    })
-})
-
-client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand()) return;
-    try {
-        const command = commands.get(interaction.commandName);
-        command?.exec(interaction);
-    } catch (error) {
-        console.error(error);
-        interaction.reply("An Error has occured.")
-    }
-})
-
-function login() {
-    if (!process.env.DISCORD_BOT_TOKEN) console.error("login Failed.")
-    client.login(process.env.DISCORD_BOT_TOKEN)
-}
- */
