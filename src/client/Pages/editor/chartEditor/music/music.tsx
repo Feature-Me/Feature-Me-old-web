f73841fd5947a6335da8966b62f19dea069f13c9
Mksk <sakasakamakki@outlook.jp>
2022-10-30 18:01:17 +0900

    updated editor

--- src/client/Pages/editor/chartEditor/music/music.tsx
+++ src/client/Pages/editor/chartEditor/music/music.tsx
@@ -18,47 +18,92 @@ const ChartEditorMusic: React.FC = () => {
     const music = chartEditorProject.project.music;
     const metadata = chartEditorProject.project.metadata;
 
-    const defaultBoxList = music.map(m=>{return{label:m.name,value:m.id}})
-    
+    const defaultBoxList = music.map(m => { return { label: m.name, value: m.id } })
+
 
     async function addMusic(files: FileList | null) {
         if (!files) return;
-        const file = files[0];
-        if (!file) return;
-        file.arrayBuffer().then(bf => {
-            const audioData = new Howl({ src:`data:${file.type};base64,${arrayBufferToBase64(bf)}`})
-            
-            audioData.once("load",()=>{
-                
-                const music: chartEditorMusic = {
-                    name: file.name,
-                    data: bf,
-                    mime: file.type,
-                    time: audioData.duration(),
-                    id:uuidv5(String(Date.now()), uuidv4())
-                }
-                
+        for (const index in files) {
+            const file = files[index];
+            if (!(file instanceof File)) return;
+            file.arrayBuffer().then(bf => {
+                const audioData = new Howl({ src: `data:${file.type};base64,${arrayBufferToBase64(bf)}` })
+
+                audioData.once("load", () => {
+
+                    const music: chartEditorMusic = {
+                        name: file.name,
+                        data: bf,
+                        mime: file.type,
+                        time: audioData.duration() * 1000,
+                        id: uuidv5(String(Date.now()), uuidv4())
+                    }
+
+                    setChartEditorProject(proj => {
+                        const musicList = cloneDeep(proj.project.music);
+                        musicList.push(music);
+                        const defaultMusic = musicList.length == 1 ? musicList[0].id : proj.project.metadata.defaultMusic;
+                        return {
+                            ...proj,
+                            project: {
+                                ...proj.project,
+                                metadata: {
+                                    ...proj.project.metadata,
+                                    defaultMusic: defaultMusic
+                                },
+                                music: musicList
+                            },
+                        }
+                    })
+                })
+                audioData.load();
+            }).catch(error => {
+                console.error(error)
                 setChartEditorProject(proj => {
-                    const musicList = cloneDeep(proj.project.music);
-                    musicList.push(music);
-                    const defaultMusic = musicList.length==1?musicList[0].id:proj.project.metadata.defaultMusic;
+                    let status = cloneDeep(proj.statusbar);
+                    let error = status.find(s=>s.id=="error");
+                    let errorIndex = status.findIndex(s=>s.id=="error");
+                    if(error){
+                        error.customValue += 1;
+                        error.label = `${error.customValue} error(s)`
+                        status[errorIndex] = error;
+                    }
+                    else {
+                        error = {
+                            id:"error",
+                            customValue:1,
+                            label:"1 error"
+                        }
+                        status.push(error);
+                    }
                     return {
                         ...proj,
-                        project: {
-                            ...proj.project,
-                            metadata:{
-                                ...proj.project.metadata,
-                                defaultMusic:defaultMusic
-                            },
-                            music: musicList
-                        },
+                        statusbar: status
                     }
                 })
             })
-            audioData.load();
-        })
+        }
 
     }
+
+    React.useEffect(()=>{
+        const times = music.map(m=>m.time);
+        const longest = Math.max(...times);
+        setChartEditorProject(proj => {
+            return {
+                ...proj,
+                project:{
+                    ...proj.project,
+                    metadata:{
+                        ...proj.project.metadata,
+                        time:longest
+                    }
+                }
+            }
+        })
+    },[music])
+
+
     return (
         <div className={style.music}>
             <h1><TranslateText content="editor.chartEditor.music.title" /></h1>
@@ -66,13 +111,13 @@ const ChartEditorMusic: React.FC = () => {
                 {/* Default Music Selector */}
                 <div className={style.interaction}>
                     <h2><TranslateText content="editor.chartEditor.music.default" /></h2>
-                    <SelectBox contents={defaultBoxList} value={defaultBoxList.find(m=>m.value==metadata.defaultMusic)||defaultBoxList[0]||{value:"",label:""}} />
+                    <SelectBox contents={defaultBoxList} value={defaultBoxList.find(m => m.value == metadata.defaultMusic) || defaultBoxList[0] || { value: "", label: "" }} />
                 </div>
                 {/*Head text and add button*/}
                 <div className={style.interaction}>
                     <h2><TranslateText content="editor.chartEditor.music.all" /> ({chartEditorProject.project.music.length})</h2>
                     <ChamferedButton><label htmlFor="editorMusicAddFileInput"><TranslateText content="editor.chartEditor.music.add" /></label></ChamferedButton>
-                    <input type="file" id="editorMusicAddFileInput" accept="audio/*" hidden onChange={e => addMusic(e.target.files)} />
+                    <input type="file" id="editorMusicAddFileInput" accept="audio/*" hidden multiple onChange={e => addMusic(e.target.files)} />
                 </div>
                 <div className={style.musicList}>
                     {
