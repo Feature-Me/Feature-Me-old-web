9cb76d05d0cb36c2b01ba2f52f2d81e11f453079
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-11-05 16:09:56 +0900

    restore changes

--- src/client/Pages/play/musicGame/solo/soloMusicGame.tsx
+++ src/client/Pages/play/musicGame/solo/soloMusicGame.tsx
@@ -1,33 +1,24 @@
 import MusicGame3D from "Features/play/musicGameEngine/3d/musicGame3d";
-import useGameLoader from "Features/play/musicGameEngine/initializer/init";
 import MusicGameUI from "Features/play/musicGameEngine/UI/musicGameUI";
 import useSeneChangeNavigation from "Hooks/scenechange/useSceneChangeNavigation";
 import React from "react";
-import { gameProps } from "Types/play/game/gameProps";
-import style from "./soloMusicGame.scss"
 
 const SoloMusicGame: React.FC = () => {
     const navigate = useSeneChangeNavigation();
-    const gameLoader = useGameLoader();
 
-    function navigateBack(e: Event) {
+    function navigateBack(e:Event){
         e.preventDefault();
-
+        
         navigate(-2);
     }
-    React.useEffect(() => {
+    React.useEffect(()=>{
         document.title = `Play solo - Feature Me`;
-        window.addEventListener("popstate", navigateBack);
-        gameLoader.load()
-        return () => {
-            window.removeEventListener("popstate", navigateBack);
-        }
-    }, [])
-
-
+        window.addEventListener("popstate",navigateBack);
+        return ()=> window.removeEventListener("popstate",navigateBack)
+    },[])
     return (
-        <div className={style.container}>
-            <MusicGame3D {...gameLoader.data} />
+        <div>
+            <MusicGame3D />
             <MusicGameUI />
         </div>
     )
--- src/client/Types/play/game/gameVariables.d.ts
+++ src/client/Types/play/game/gameVariables.d.ts
@@ -5,6 +5,7 @@ interface musicGameVariablesType {
     activeRange: number
     activeNotes: Array<note>
     delay: number
+    ready: boolean
     inputs: {
         position: "left" | "right" | "center",
         lanes: [boolean, boolean, boolean, boolean];
