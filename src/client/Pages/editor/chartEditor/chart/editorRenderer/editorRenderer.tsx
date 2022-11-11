31c6514f5e65d7849baadfca9c583da3bc127288
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-11-11 18:54:23 +0900

    updated chart editor renderer

--- src/client/Pages/editor/chartEditor/chart/editorRenderer/editorRenderer.tsx
+++ src/client/Pages/editor/chartEditor/chart/editorRenderer/editorRenderer.tsx
@@ -1,15 +1,24 @@
+import useSeneChangeNavigation from "Hooks/scenechange/useSceneChangeNavigation";
 import * as PIXI from "pixi.js";
 import React from "react";
+import { useNavigate } from "react-router";
+import { useRecoilState } from "recoil";
+import { chartProjectState } from "State/editor/chartProjectState";
 
 import style from "./editorRenderer.scss"
 
 const ChartEditorRenderer:React.FC<{}> = (props) =>{
+    const sceneChange = useSeneChangeNavigation();
+    const [chartProject, setChartProject] = useRecoilState(chartProjectState);
+    
+    const editorContainerRef = React.useRef<HTMLDivElement>(null);
+
     return(
         <div className={style.editorRenderer}>
             <div className={style.sideBar}>
 
             </div>
-            <div className={style.canvasContainer}>
+            <div className={style.editorContainer} ref={editorContainerRef}>
 
             </div>
         </div>
