cd6775e734e8867e96fa10ba26baad09e728e62e
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-11-14 23:43:56 +0900

    fixed score rank

--- src/client/Utils/getScoreRankFromScore/getScoreRank.ts
+++ src/client/Utils/getScoreRankFromScore/getScoreRank.ts
@@ -2,11 +2,11 @@ const scoreRanks: scoreRanks = [
     { label: "SSS+", min: 990000, max: 1000000 },
     { label: "SSS", min: 980000, max: 990000 },
     { label: "SS+", min: 970000, max: 980000 },
-    { label: "SS", min: 950000, max: 97000 },
+    { label: "SS", min: 950000, max: 970000 },
     { label: "S+", min: 940000, max: 950000 },
     { label: "S", min: 900000, max: 940000 },
     { label: "AAA+", min: 890000, max: 900000 },
-    { label: "AAA", min: 870000, max: 89000 },
+    { label: "AAA", min: 870000, max: 890000 },
     { label: "AA+", min: 860000, max: 870000 },
     { label: "AA", min: 840000, max: 860000 },
     { label: "A+", min: 830000, max: 840000 },
@@ -17,7 +17,8 @@ const scoreRanks: scoreRanks = [
 ]
 
 function getScoreRankFromScore(score: number) {
-    const rank = scoreRanks.find(s => s.max >= score && s.min < score) || {label:"unkown",min:0,max:0}
+    const rank = scoreRanks.find(s => s.max >= score && s.min < score) || {label:"unkown",min:0,max:0};
+
     return rank.label;
 }
 
