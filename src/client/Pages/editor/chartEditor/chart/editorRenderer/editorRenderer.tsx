234edbc91c5271453aff6056e8854242bd1281b6
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-11-15 17:44:16 +0900

    added vertical line

--- src/client/Pages/editor/chartEditor/chart/editorRenderer/editorRenderer.tsx
+++ src/client/Pages/editor/chartEditor/chart/editorRenderer/editorRenderer.tsx
@@ -17,6 +17,7 @@ const ChartEditorRenderer: React.FC<{}> = (props) => {
     const Application = new PIXI.Application(
         {
             backgroundColor: 0x414040,
+            resolution:1,
         }
     )
 
@@ -24,38 +25,46 @@ const ChartEditorRenderer: React.FC<{}> = (props) => {
         if (!canvasContainerRef.current) return;
         canvasContainerRef.current.appendChild(Application.view);
         setSize();
-        setVerticalLine()
-        window.addEventListener("wheel",scrollCanvas)
+        window.addEventListener("wheel", scrollCanvas)
+        window.addEventListener("resize", setSize)
     }, [])
-    function scrollCanvas(e:WheelEvent){
-        if(!canvasContainerRef.current)return;
-        if(e.deltaY==0)return;
-        canvasContainerRef.current.scrollBy(e.deltaY,0)
-    }
-    function setSize() {
+    function scrollCanvas(e: WheelEvent) {
         if (!canvasContainerRef.current) return;
-        
-        Application.renderer.resize(beatCount*48+32||2000, canvasContainerRef.current.clientHeight)
+        if (e.deltaY == 0) return;
+        canvasContainerRef.current.scrollBy(e.deltaY, 0)
     }
 
-    function setVerticalLine(){
-        
+    function setSize() {
+        if (!canvasContainerRef.current) return;
+        console.log(beatCount);
+        Application.renderer.resize(beatCount * 64 + 32 || 2000, canvasContainerRef.current.clientHeight)
+        setVerticalLine();
+    }
+
+    function setVerticalLine() {
+
         //left and right margin : 16px
         for (let i = 0; i < beatCount; i++) {
-            const x = 48 * i + 16
+            const x = 64 * i + 16
             const y = Application.renderer.height;
             const graphics = new PIXI.Graphics();
-            graphics.lineStyle(2,0xaaaaaa);
-            graphics.moveTo(x,16);
-            graphics.lineTo(x,y-16);
-            const style= new PIXI.TextStyle({
+            graphics.lineStyle(2, 0xaaaaaa);
+            graphics.moveTo(x, 16);
+            graphics.lineTo(x, y - 16);
+            const style = new PIXI.TextStyle({
                 fontSize: 12,
-                fill:'#f5f5f5'
+                fill: '#f5f5f5'
             });
-            const text = new PIXI.Text(i+1, style);
+            const text = new PIXI.Text(i + 1, style);
             text.x = x
             text.y = 0
-            Application.stage.addChild(graphics,text)
+            Application.stage.addChild(graphics, text)
+            for (let i = 0; i < 3; i++) {
+                const deltaX = x + ((i + 1) * 16)
+                graphics.lineStyle(1, 0x909090);
+                graphics.moveTo(deltaX, 16)
+                graphics.lineTo(deltaX, y - 16)
+            }
         }
     }
 
@@ -65,7 +74,6 @@ const ChartEditorRenderer: React.FC<{}> = (props) => {
 
             </div>
             <div className={style.canvasContainer} ref={canvasContainerRef}>
-
             </div>
         </div>
     )
