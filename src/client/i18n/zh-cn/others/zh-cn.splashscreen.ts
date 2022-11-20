09e6b3643bbb11b9da67eea5e08c649f7aea7e92
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-11-20 18:23:22 +0900

    updated splash screen translate

---
+++ src/client/i18n/zh-cn/others/zh-cn.splashscreen.ts
@@ -0,0 +1,8 @@
+const ZHCNSplashScreenTranslation = {
+    "caution": {
+        "title": "注意",
+        "description": "闪光或光刺激可能导致眼睛疼痛或抽搐。如果你有症状，请采取预防措施。\n避免长时间玩耍,适度休息。如果你感到任何身体症状,如头痛、头晕或疲劳,请立即停止游戏。如果症状没有改善,请咨询医生。\n玩游戏时一定要遵守规则、礼仪和使用条款,确保所有玩家都能舒适地游戏。"
+    }
+}
+
+export default ZHCNSplashScreenTranslation;
