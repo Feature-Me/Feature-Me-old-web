1ca6d6ff07d5ca6c0ac5478140cf9da95d892052
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-11-22 00:31:47 +0900

    reverted getNearest

--- src/client/Utils/getNearest/getNearest.ts
+++ src/client/Utils/getNearest/getNearest.ts
@@ -3,12 +3,6 @@ import insertionSort from "Utils/Sort/insertionSort";
 
 function getNearest(value: number, numbers: Array<number>): number {
 
-    return numbers.length < 8192 ?
-        getNearestWithoutSort(value, numbers) :
-        getNearestWithSort(value, numbers)
-}
-
-function getNearestWithoutSort(value: number, numbers: Array<number>): number {
     if (numbers.find(n => n == value)) return value;
 
     let nearest = 0;
@@ -17,22 +11,6 @@ function getNearestWithoutSort(value: number, numbers: Array<number>): number {
     });
 
     return nearest
-
-}
-
-function getNearestWithSort(value: number, numbers: Array<number>): number {
-    if (numbers.find(n => n == value)) return value;
-    numbers = insertionSort(numbers);
-    let numberSet = new Set(numbers);
-    let nearest = 0;
-    let diff = Infinity;
-    numberSet.forEach(n => {
-        const currentDiff = Math.abs(value - n);
-        if (currentDiff > diff) return;
-        diff = currentDiff;
-        if (Math.abs(value - nearest) > currentDiff) nearest = n
-    });
-    return nearest;
 }
 
 export default getNearest
