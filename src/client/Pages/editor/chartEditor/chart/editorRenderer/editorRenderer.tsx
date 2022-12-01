82b186b11859894b0228ce77080bca6216e50b50
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-01 18:13:53 +0900

    updated editor renderer

--- src/client/Pages/editor/chartEditor/chart/editorRenderer/editorRenderer.tsx
+++ src/client/Pages/editor/chartEditor/chart/editorRenderer/editorRenderer.tsx
@@ -6,7 +6,7 @@ import html2canvas from "html2canvas";
 import * as PIXI from "pixi.js";
 import React from "react";
 import { useNavigate } from "react-router";
-import { useRecoilState, useSetRecoilState } from "recoil";
+import { useRecoilState} from "recoil";
 import { chartProjectState } from "State/editor/chartProjectState";
 import getNearest from "Utils/getNearest/getNearest";
 
@@ -28,7 +28,7 @@ import chartEditorEditingNotesState from "State/editor/editorState";
 const ChartEditorRenderer = React.memo((props:{current: chartEditorChart, setCurrent: React.Dispatch<React.SetStateAction<chartEditorChart>>}) => {
     const sceneChange = useSeneChangeNavigation();
     const [chartProject, setChartProject] = useRecoilState(chartProjectState);
-    const setChartEditorEditingNotes = useSetRecoilState(chartEditorEditingNotesState);
+    const [chartEditorEditingNotes,setChartEditorEditingNotes] = useRecoilState(chartEditorEditingNotesState);
     const [scale, setScale] = React.useState(1);
     const [quantize, setQuantize] = React.useState(4);
     const [isBeatBased, setIsBeatBased] = React.useState(true);
@@ -39,6 +39,7 @@ const ChartEditorRenderer = React.memo((props:{current: chartEditorChart, setCur
     const [verticalAnchor, setVerticalAnchor] = React.useState<Array<number>>([]);
     const [horizonalAnchor, setHorizonalAnchor] = React.useState<Array<{ name: string, position: number, type: string, lane: number | string }>>([]);
     const [selectedMode, setSelectedMode] = React.useState<{ name: string, type: string }>({ name: "cursor", type: "cursor" });
+    const [noteTypeIntegrity, setNoteTypeIntegrity] = React.useState(false);
     const canvasContainerRef = React.useRef<HTMLDivElement>(null);
     const editorCanvasRef = React.useRef<HTMLDivElement>(null);
     let beatCount = Math.ceil((chartProject.project.metadata.time || 60000) / (chartProject.project.metadata.bpm || 120) / 4)
@@ -66,7 +67,6 @@ const ChartEditorRenderer = React.memo((props:{current: chartEditorChart, setCur
     function wheelAction(e: WheelEvent) {
         if (e.altKey) {
             console.log(e.deltaX);
-
             if (e.deltaY > 0) setScale(x => Math.min(x + 0.1, 20))
             else setScale(x => Math.max(x - 0.1, 0.2))
 
@@ -85,9 +85,15 @@ const ChartEditorRenderer = React.memo((props:{current: chartEditorChart, setCur
         const nearestX = getNearest(x, verticalAnchor)
         if (nearestX != 0) setCurosrPosX(nearestX);
         //get relative position y (%)
-        const y = (e.pageY - boundingBox.top - 32) / boundingBox.height * 100
+        const y = (e.pageY - (boundingBox.top+32)) / (boundingBox.height - 36) * 100
         const nearestY = getNearest(y, horizonalAnchor.map(a => a.position));
         if (nearestY != 0) setCurosrPosY(nearestY);
+        updateCursorNoteIcon(nearestY||cursorPosY);
+    }
+    function updateCursorNoteIcon(nearestY:number){
+        const type = selectedMode.type;
+        const lineType = horizonalAnchor.find(h=>h.position==nearestY);
+        setNoteTypeIntegrity(type == lineType?.type);
     }
 
     function setSize() {
@@ -104,10 +110,16 @@ const ChartEditorRenderer = React.memo((props:{current: chartEditorChart, setCur
         json5.parse(props.current.data)
     }
 
+    function addNote(e:React.MouseEvent<HTMLDivElement,MouseEvent>){
+        if(!noteTypeIntegrity) return;
+
+
+    }
+
     React.useEffect(() => {
         if (!canvasContainerRef.current) return;
         setSize();
-        canvasContainerRef.current.addEventListener("wheel", wheelAction)
+        canvasContainerRef.current.addEventListener("wheel", wheelAction);
         parseChart
         return () => {
             if (!canvasContainerRef.current) return;
@@ -203,33 +215,38 @@ const ChartEditorRenderer = React.memo((props:{current: chartEditorChart, setCur
                             })
                         }
                     </div>
-                    <div className={style.lineContainer}>
-                        <div className={style.vLineContainer}>
-                            {
-                                verticalAnchor.map((value, index) => {
-                                    let flag = index % quantize == 0 ? true : false;
-                                    if (quantize == 1) flag = true;
-                                    return (
-                                        <div className={`${style.verticalLine} ${flag ? style.baseLine : style.nonBaseLine}`} style={{ left: `${value}px` }} key={index} />
-                                    )
-                                })
-                            }
-                        </div>
-                        <div className={style.hLineContainer}>
-                            {
-                                horizonalAnchor.map((value, index) => {
-                                    return (
-                                        <div className={style.horizonalLine} style={{ top: `${value.position}%` }} key={index} />
-                                    )
-                                })
-                            }
-                        </div>
+                    <div className={style.editorView}>
+                        <div className={style.lineContainer}>
+                            <div className={style.vLineContainer}>
+                                {
+                                    verticalAnchor.map((value, index) => {
+                                        let flag = index % quantize == 0 ? true : false;
+                                        if (quantize == 1) flag = true;
+                                        return (
+                                            <div className={`${style.verticalLine} ${flag ? style.baseLine : style.nonBaseLine}`} style={{ left: `${value}px` }} key={index} />
+                                        )
+                                    })
+                                }
+                            </div>
+                            <div className={style.hLineContainer}>
+                                {
+                                    horizonalAnchor.map((value, index) => {
+                                        return (
+                                            <div className={style.horizonalLine} style={{ top: `${value.position}%` }} key={index} />
+                                        )
+                                    })
+                                }
+                            </div>
 
-                        <div className={style.cursorLineContainer}>
+                        </div>
+                        <div className={style.noteIconContainer} onClick={addNote}>
+
+                        </div>
+                        <div className={style.cursorObjectContainer}>
                             <div className={`${style.cursorVLine} ${style.cursorLine}`} style={{ left: `${cursorPosX}px` }}></div>
                             <div className={`${style.cursorHLine} ${style.cursorLine}`} style={{ top: `${cursorPosY}%` }}></div>
+                            <div className={`${style.noteIcon} ${style[selectedMode.name]} ${!noteTypeIntegrity ? style.inactive : ""}`} style={{ left: `${cursorPosX}px`, top: `${cursorPosY}%` }} />
                         </div>
-
                     </div>
                 </div>
             </div>
