2e0d3bab09ac3aa491aa7e8f26b2834c5ca35566
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2022-12-08 17:43:32 +0900

    updated etiror note type

--- src/client/Types/editor/chartEditorNote.d.ts
+++ src/client/Types/editor/chartEditorNote.d.ts
@@ -1,5 +1,11 @@
 interface chartEditorNote{
-    
+    type:"tap"|"damageTap"|"hold"|"bright"|"flick"|"seed"|"camera"|"speed"|"text"
+    time:number
+    lane:string|number
+}
+interface chartEditorHoldNote extends chartEditorNote {
+    type:"hold"
+    duration:number
 }
 interface chartEditorEditingNotesState {
     notes:Array<chartEditorNote>
