5e0764e6b395ade77ff2926bca9e0b9e00ffd9cb
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-13 23:11:11 +0900

    updated utils

--- src/client/Utils/Storage/LocalStorage/initLocalStorage.tsx
+++ src/client/Utils/Storage/LocalStorage/initLocalStorage.tsx
@@ -1,20 +1,21 @@
-import React from "react";
 import { toast } from "react-toastify";
-import TranslateText from "Components/TranslateText/TranslateText";
 import { enviroment, resourcesDownloaded, DBVersion, gameConfig, musicSelect } from "./defaultValue";
+import fixKeybind from "Utils/fixKeybind/fixKeybind";
 
 function initLocalStorage(): void {
     try {
-        
-        localStorage.setItem("environment", JSON.stringify({ ...enviroment, ...(JSON.parse(localStorage.getItem("environment")||"{}")) }));
+
+        localStorage.setItem("environment", JSON.stringify({ ...enviroment, ...(JSON.parse(localStorage.getItem("environment") || "{}")) }));
         localStorage.setItem("resourcesDownloaded", JSON.stringify({ ...resourcesDownloaded, ...(JSON.parse(localStorage.getItem("resourcesDownloaded") || "{}")) }));
         localStorage.setItem("DBVersion", JSON.stringify({ ...DBVersion, ...(JSON.parse(localStorage.getItem("DBVersion") || "{}")) }));
         localStorage.setItem("gameConfig", JSON.stringify({ ...gameConfig, ...(JSON.parse(localStorage.getItem("gameConfig") || "{}")) }));
         localStorage.setItem("musicSelect", JSON.stringify({ ...musicSelect, ...(JSON.parse(localStorage.getItem("musicSelect") || "{}")) }));
-    
-} catch (error) {
-    console.error(error);
-    toast.error(`${error}`);
+
+        fixKeybind();
+
+    } catch (error) {
+        console.error(error);
+        toast.error(`${error}`);
     }
 }
 
