e9b0d08257094a840b6ddeccd5fbfc303a574b48
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-15 20:22:42 +0900

    fixed fix keybind function

--- src/client/Utils/fixKeybind/fixKeybind.tsx
+++ src/client/Utils/fixKeybind/fixKeybind.tsx
@@ -2,15 +2,9 @@ function fixKeybind() {
     if (!localStorage.getItem("gameConfig")) return;
     const config = JSON.parse(localStorage.getItem("gameConfig") || "{}");
     if (!("length" in config.gameplay.key)) {
-        let newConfig = new Array(7);
-        for (const key in config.gameplay.key) {
-            if (Object.prototype.hasOwnProperty.call(config.gameplay.key, key)) {
-                const element = config.gameplay.key[key];
-                newConfig[Number(key)] = element;
-            }
-        }
+        let newConfig = Object.values(config.gameplay.key);
         config.gameplay.key = newConfig;
-        localStorage.setItem("gameConfig",JSON.stringify(config))
+        localStorage.setItem("gameConfig", JSON.stringify(config))
     }
 }
 
