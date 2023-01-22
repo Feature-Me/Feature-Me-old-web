045b289ba47987c8f5545310908e884f210ffe57
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-23 01:16:27 +0900

    add : button

---
+++ src/client/Components/Button/chamferedButton/chamferedButton.tsx
@@ -0,0 +1,39 @@
+import * as solid from "solid-js";
+
+import style from "./chamferedButton.module.scss";
+
+interface buttonPropsType extends solid.JSX.ButtonHTMLAttributes<HTMLButtonElement> {
+    accentColor?: string
+}
+
+
+const ChamferedButton: solid.Component<buttonPropsType> = (props) => {
+
+    let chamferedButtonRef: HTMLButtonElement|undefined;
+
+    //if there is custom accent color,set lisner to override color
+    solid.createEffect(() => {
+        if(!chamferedButtonRef) return;
+        chamferedButtonRef.addEventListener("mouseenter", () => setHoverAction(true));
+        chamferedButtonRef.addEventListener("mouseleave", () => setHoverAction(false));
+        return () => {
+            if (!chamferedButtonRef) return;
+            chamferedButtonRef.removeEventListener("mouseenter", () => setHoverAction(true));
+            chamferedButtonRef.removeEventListener("mouseleave", () => setHoverAction(false));
+        }
+    }, [])
+
+    function setHoverAction(active: boolean) {
+        if (!chamferedButtonRef || !props.accentColor) return;
+        if (active) chamferedButtonRef.style.backgroundColor = props.accentColor;
+        else chamferedButtonRef.style.backgroundColor = "";
+    }
+
+    return (
+        <button onClick={props.onClick} class={`${style.chamferedbutton} ${props.class || ""}`} ref={chamferedButtonRef} >
+            {props.children}
+        </button>
+    )
+}
+
+export default ChamferedButton;
