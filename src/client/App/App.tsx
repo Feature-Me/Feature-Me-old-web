d519d9280a4640a708dfb5c0438794f6f0edec0b
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-23 01:17:50 +0900

    addd : crash handler to app

--- src/client/App/App.tsx
+++ src/client/App/App.tsx
@@ -1,14 +1,16 @@
-import i18n from 'i18n/i18n';
+import i18n from "i18n/i18n";
 import I18nProvider from "Components/i18nProvider/i18nProvider";
 import i18next from "i18next";
-import * as solid from 'solid-js';
+import * as solid from "solid-js";
 
 import version from "Assets/StaticInfo/version.json";
 
 import style from "./App.module.scss"
+import Loading from "Components/LoadingCover/loading";
+import CrashHandler from "Components/ErrorBoundary/ApplicatonCrashHandler/crashHandler";
 
 const App: solid.Component = () => {
-    const [loaded, setLoaded] = solid.createSignal(false);
+    const [loaded, setLoaded] = solid.createSignal(true);
     solid.onMount(async () => {
         await i18n;
         setLoaded(true);
@@ -21,13 +23,15 @@ const App: solid.Component = () => {
     });
 
     return (
-        <solid.Show when={loaded()}>
-            <I18nProvider i18n={i18next}>
-                <div class={style.app}>
-                    Feature Me {version.version} <br /> With solid js
-                </div>
-            </I18nProvider>
-        </solid.Show>
+        <solid.ErrorBoundary fallback={(err, reset) => <CrashHandler error={err} reset={reset} />}>
+            <solid.Show when={loaded()} fallback={<Loading />}>
+                <I18nProvider i18n={i18next}>
+                    <div class={style.app}>
+                        Feature Me {version.version} <br /> With solid js
+                    </div>
+                </I18nProvider>
+            </solid.Show>
+        </solid.ErrorBoundary>
     );
 }
 
