e8db40832cec50f400c9c5b204831942277fcd53
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-24 00:44:17 +0900

    update : router

--- src/client/Router/router.tsx
+++ src/client/Router/router.tsx
@@ -1,9 +1,17 @@
 import * as solid from "solid-js";
+import { Routes, Route } from "@solidjs/router";
 
-const Router:solid.Component = () => {
-    return(
-        <div></div>
+import Initializer from "Pages/initializer/initializer";
+import SplashScreen from "Pages/splash/splashScreen";
+
+
+const PageRouter: solid.Component = () => {
+    return (
+        <Routes>
+            <Route path={"/"} element={<Initializer />} />
+            <Route path={"/splash"} element={<SplashScreen />} />
+        </Routes>
     )
 }
 
-export default Router;
+export default PageRouter;
