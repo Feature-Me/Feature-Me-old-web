32a2801280de6271f1fe5ce62ac5f77554acfbf7
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-24 00:41:22 +0900

    update : import language and some JSX

--- src/client/App/App.tsx
+++ src/client/App/App.tsx
@@ -2,12 +2,16 @@ import i18n from "i18n/i18n";
 import I18nProvider from "Components/i18nProvider/i18nProvider";
 import i18next from "i18next";
 import * as solid from "solid-js";
+import { Router } from "@solidjs/router";
 
-import version from "Assets/StaticInfo/version.json";
+import PageRouter from "Router/router";
 
-import style from "./App.module.scss"
 import Loading from "Components/LoadingCover/loading";
 import CrashHandler from "Components/ErrorBoundary/ApplicatonCrashHandler/crashHandler";
+
+
+import style from "./App.module.scss";
+import "./style.scss";
 
 const App: solid.Component = () => {
     const [loaded, setLoaded] = solid.createSignal(true);
@@ -17,6 +21,10 @@ const App: solid.Component = () => {
     });
 
     solid.createEffect(() => {
+        if (!loaded()) return;
+        console.log(`Language : ${i18next.language}`);
+        console.log(`All avaliable languages : ${i18next.languages}`);
+
         const environment = JSON.parse(localStorage.getItem("environment") || "{}");
         environment.language = i18next.language;
         localStorage.setItem("environment", JSON.stringify(environment));
@@ -26,9 +34,11 @@ const App: solid.Component = () => {
         <solid.ErrorBoundary fallback={(err, reset) => <CrashHandler error={err} reset={reset} />}>
             <solid.Show when={loaded()} fallback={<Loading />}>
                 <I18nProvider i18n={i18next}>
-                    <div class={style.app}>
-                        Feature Me {version.version} <br /> With solid js
-                    </div>
+                    <Router>
+                        <div class={style.app}>
+                            <PageRouter />
+                        </div>
+                    </Router>
                 </I18nProvider>
             </solid.Show>
         </solid.ErrorBoundary>
