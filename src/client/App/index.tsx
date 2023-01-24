490c440e4b45650d9ead432ffc0d17a2750f1ff1
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-25 00:22:49 +0900

    update : removed spaces in init func code

--- src/client/App/index.tsx
+++ src/client/App/index.tsx
@@ -11,14 +11,9 @@ function render() {
 }
 
 function init() {
-
-
     console.log("Feature Me initialized. the Game is Ready!");
     console.log("%c Hold Up! \n Please be careful when you paste anything to console.\n Attackers may steal your information. ", "color:#ffa69f;background-color:#633023;font-size:16px;");
     console.log(`%c Running Feature Me ${version.version}.Build:${0}. `, "color:#a0d6a6;background-color:#2d3c2e;font-size:16px;");
     console.log(`%c SolidJS 1.6.9 \n THREE.JS r${REVISION} `, "color:#f3c0e6;background-color:#3c2d38;font-size:16px;")
-
     render();
-
-
 }
