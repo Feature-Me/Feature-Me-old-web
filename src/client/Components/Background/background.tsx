615db2fd7c04ee3b47a73997a1fc5da3e59b5509
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-25 00:20:33 +0900

    add : background component

---
+++ src/client/Components/Background/background.tsx
@@ -0,0 +1,39 @@
+import * as solid from "solid-js";
+
+import style from "./background.module.scss";
+
+import fallbackImage from "Assets/Images/tidal wreck background.png";
+import { Transition } from "solid-transition-group";
+import { renderBackground } from "State/backgroundState";
+const Background: solid.Component = (props) => {
+
+    function enter(el: Element, done: () => void) {
+        const animation = el.animate([{ opacity: 0 }, { opacity: 1 }], {
+            duration: 200
+        });
+        animation.finished.then(done);
+    }
+
+    function exit(el: Element, done: () => void) {
+        const animation = el.animate([{ opacity: 1 }, { opacity: 0 }], {
+            duration: 200
+        });
+        animation.finished.then(done);
+
+    }
+
+    return (
+        <div class={style.background}>
+            <Transition onEnter={enter}
+                onExit={exit}>
+                {
+                    renderBackground() &&
+                    <img src={fallbackImage} alt="Tidal Wreck" class={style.fallback} />
+                }
+            </Transition>
+
+        </div>
+    )
+}
+
+export default Background;
