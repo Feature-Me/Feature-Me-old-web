200ae2345fb400767313efa6de08661e51a89243
Mksk <81460622+Mkskdesu@users.noreply.github.com>
2023-01-26 01:08:48 +0900

    modified : make db init script to use dexie

--- src/client/Utils/Storage/database/initDatabase.tsx
+++ src/client/Utils/Storage/database/initDatabase.tsx
@@ -1,51 +1,32 @@
-import React from "react";
-import { toast } from "react-toastify";
+import Dexie from "dexie";
+import compareVersions from "compare-versions";
 
-import TranslateText from "Components/TranslateText/TranslateText";
+import databaseInfo from "Assets/StaticInfo/databaseinfo.json";
+import DatabaseError from "Utils/Errors/DatabaseError";
 
-import databaseInfo from "Config/databaseinfo.json";
-
-let DBVersion = (localStorage.getItem("DBVersion") && JSON.parse(localStorage.getItem("DBVersion")!).version + 1) || 1;
 function initDatabase() {
-    const databaseRequest = indexedDB.open(databaseInfo.DBName, DBVersion);
-    databaseRequest.onupgradeneeded = () => {
-        console.log("Database Upgraded");
-        try {
-            const db = databaseRequest.result;
-            if(!db.objectStoreNames.contains(databaseInfo.backgroundStore))
-                db.createObjectStore(databaseInfo.backgroundStore, { keyPath: "name" });
+    try {
+        if (compareVersions(databaseInfo.version, JSON.parse(localStorage.getItem("DBVersion") || '{version:"0.0.0"}').version) != 1) return;
+        console.log("initializing database");
 
-            if(!db.objectStoreNames.contains(databaseInfo.behaviorStore))
-                db.createObjectStore(databaseInfo.behaviorStore, { keyPath: "name" });
+        const db = new Dexie(databaseInfo.DBName);
 
-            if(!db.objectStoreNames.contains(databaseInfo.soundEffectStore))
-                db.createObjectStore(databaseInfo.soundEffectStore, { keyPath: "name" });
-
-            if(!db.objectStoreNames.contains(databaseInfo.fontStore))
-                db.createObjectStore(databaseInfo.fontStore, { keyPath: "name" });
-
-            if(!db.objectStoreNames.contains(databaseInfo.musicStore))
-                db.createObjectStore(databaseInfo.musicStore, { keyPath: "name" });
-
-            if(!db.objectStoreNames.contains(databaseInfo.storyStore))
-                db.createObjectStore(databaseInfo.storyStore, { keyPath: "id" });
-
-            if(!db.objectStoreNames.contains(databaseInfo.replayStore))
-                db.createObjectStore(databaseInfo.replayStore, { keyPath: "id",});
-            
-            if(!db.objectStoreNames.contains(databaseInfo.editorStore))
-                db.createObjectStore(databaseInfo.editorStore, { keyPath: "id"});
-
-            if(!db.objectStoreNames.contains(databaseInfo.practiceAssetStore))
-                db.createObjectStore(databaseInfo.practiceAssetStore,{keyPath:"name"});
-
-            db.close();
-            localStorage.setItem("DBVersion", JSON.stringify({ version: DBVersion, initialized: true, updated: Date.now() }));
-        } catch (error) {
-            console.error(error);
-            toast.error(`${<TranslateText content= { "resourcesManager.database.notifications.initializingFailed"} />} : ${ error } `);
+        let stores: { [key: string]: string } = {};
+        for (const key of databaseInfo.databases) {
+            stores[key] = "++index";
         }
+
+        db.version(db.verno + 1).stores({ ...stores });
+        db.open().then((database) => {
+            database.close();
+        }).catch(error => { throw new DatabaseError("cannot open database") })
+
+        localStorage.setItem("DBVersion", JSON.stringify({ version: databaseInfo.version, initialized: true, updated: Date.now() }));
+    } catch (error) {
+        console.error(error);
+        throw error;
     }
 }
+
 
 export default initDatabase;
