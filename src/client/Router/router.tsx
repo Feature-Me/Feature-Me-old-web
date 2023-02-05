d0e21a9917a51faf8e12debad5d07744e5ddd200
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-05 18:31:17 +0900

    update : router

--- src/client/Router/router.tsx
+++ src/client/Router/router.tsx
@@ -4,6 +4,7 @@ import { Routes, Route, Navigate } from "@solidjs/router";
 import Initializer from "Pages/appInitializer/appInitializer";
 import SplashScreen from "Pages/splash/splashScreen";
 import Loader from "Pages/appLoader/loader";
+import Title from "Pages/title/title";
 
 
 const PageRouter: solid.Component = () => {
@@ -12,6 +13,8 @@ const PageRouter: solid.Component = () => {
             <Route path={"/"} element={<Navigate href={"/splash"} />} />
             <Route path={"/splash"} element={<SplashScreen />} />
             <Route path={"/load"} element={<Loader />} />
+            <Route path={"/title"} element={<Title />} />
+            <Route path={"/setup"} element={<div />} />
         </Routes>
     )
 }
