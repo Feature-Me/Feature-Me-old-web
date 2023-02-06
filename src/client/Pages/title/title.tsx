4601cd1b70d3dc9442b0c26c28a69c38202767ec
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-07 00:06:14 +0900

    update : title page

--- src/client/Pages/title/title.tsx
+++ src/client/Pages/title/title.tsx
@@ -12,6 +12,7 @@ import background from "Assets/Images/tidal_wreck_far_camera.png";
 
 const Title: solid.Component = () => {
     const navigate = useNavigate();
+    let containerRef: HTMLDivElement | undefined
 
     function closeWindow() {
         window.close();
@@ -23,9 +24,17 @@ const Title: solid.Component = () => {
         navigate("/home");
     }
 
+    useBeforeLeave(async (e) => {
+        if (!e.defaultPrevented) e.preventDefault();
+        if (!containerRef) return;
+        containerRef.style.animation = "fadeOut 0.3s linear forwards";
+        setTimeout(() => {
+            e.retry(true);
+        }, 3000)
+    })
 
     return (
-        <div class={style.title}>
+        <div class={style.title} ref={containerRef}>
             <div class={style.background}>
                 <GlitchImage src={background} />
             </div>
@@ -44,7 +53,7 @@ const Title: solid.Component = () => {
             </div>
             <div class={style.footer}>
                 <p>Feature Me {version.version} Mksk and Rae the Feature Me Project <br /> ©{new Date().getFullYear()} Feature Me All rights reserved.</p>
-                <div class={`iconWrapper ${style.settings}`} onClick={()=>{}}>
+                <div class={`iconWrapper ${style.settings}`} onClick={() => { }}>
                     <BsGear class={style.settingsIcon} />
                 </div>
             </div>
