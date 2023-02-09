ba3c4176f2de357aff6f7f37ef7cf7a3113311cd
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-09 17:50:08 +0900

    update : setup page

--- src/client/Pages/setup/setup.tsx
+++ src/client/Pages/setup/setup.tsx
@@ -4,27 +4,25 @@ import * as solid from "solid-js";
 import style from "./setup.module.scss"
 import sleep from "Utils/sleep/sleep";
 import SetupCautions from "./contents/cautions/cautions";
+import SetupTerms from "./contents/terms/terms";
 
 
 const Setup: solid.Component = () => {
     const navigate = useNavigate();
     const [fadeOut, setFadeOut] = solid.createSignal<boolean>(false);
-    const [pageCount, setPageCount] = solid.createSignal<number>(0);
-    const pages = [SetupCautions];
-    const [currentPage, setCurrentPage] = solid.createSignal(pages[pageCount()])
 
-    useBeforeLeave(async (e) => {
+/*     useBeforeLeave(async (e) => {
         if (!e.defaultPrevented) e.preventDefault();
         await sleep(1000);
         e.retry(true);
-    })
-
+    }) */
 
     return (
         <div class={style.setup} classList={{ blackOut: fadeOut() }}>
             <Routes>
                 <Route path={"/"} element={<Navigate href={"./caution"} />} />
                 <Route path={"/caution"} element={<SetupCautions />} />
+                <Route path={"/terms"} element={<SetupTerms />} />
             </Routes>
         </div>
     )
