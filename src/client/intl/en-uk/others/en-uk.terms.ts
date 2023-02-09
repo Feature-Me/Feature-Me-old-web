1817cba5cc43a622c38beb316ed379c5cbb8d992
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-09 17:46:40 +0900

    update : terms

--- src/client/intl/en-uk/others/en-uk.terms.ts
+++ src/client/intl/en-uk/others/en-uk.terms.ts
@@ -1,46 +1,47 @@
 const ENUKTermsTranslation = {
     "title": "Terms of Service",
-    "content":`
-    1. Agreement to the Terms of Use
-    By agreeing to the Terms of Use, you agree to use the Service. If you continue to use the service, you are deemed to have agreed to these Terms of Use.
+    "content": `
+    1. Acceptance of Terms of Use
+    By agreeing to the Terms of Use, you may use Feature Me (hereinafter referred to as "the Service"). By agreeing to the Terms of Use, you may use the Service.
     
     2.Changes to the Terms of Use
-    The Service may amend these Terms of Use from time to time as necessary. The amended Terms and Conditions shall come into effect as soon as they are published on the Service.
+    The Service may modify these Terms of Use from time to time as necessary. The amended Terms of Use shall become effective upon publication on the Service.
 
     3. Contents of the Service
-    The Service provides the following services to the User
-    3.1. Playing music games. 
+    The Service provides the following services to users.
+    3.1. Provision of music game play
     3.2. Interaction with other companies using online functions.
-    3.3. Creation and sharing of original content.
+    3.3. Creation and sharing of original contents.
 
-    4. Responsibilities of the user
-    In using the Service, the User shall not engage in any of the following acts.
-    Slander, selling, lending, transferring, leasing or contracting with others using the online functions of the Service. 4.2.
-    Unlawful acts against others using the Services. 4.2.
-    Unauthorised diversion of the contents of the Service (whether commercial or non-commercial).
+    4. User's Responsibility
+    Users shall not engage in any of the following acts when using the Service.
+    4.1. Slandering, selling, lending, transferring, leasing, or contracting with others using the online functions of the Service.
+    4.2. Any illegal acts against others using the Service.
+    Unauthorized use of the Service (unauthorized modification programs, cheats, etc.).
+    4.4. Unauthorized diversion of the contents of the Service (whether commercial or non-commercial).
 
-    5.Complaints and consultations regarding the Service
-    Please use the in-game report function for complaints, consultations and reports regarding the Service.
+    5. Additional information about the Service
+    If you have any complaints, concerns, or reports regarding the Service, please refer to the in-game report function or the project page on GitHub or Scrapbox.
 
-    6.Copyright in the Service
-    All copyrights relating to the Service and all content relating to the Service belong to the copyright holders of the Service.
-    6.1. Games in general: Mksk
-    6.2. Original music for the Service: Raetan, Goners
-    6.3.Original images of the Service: Mksk,Raetan,Goners
+    6.Copyrights for the Service
+    Copyrights to the Service for all officially distributed contents belong to the copyright holders of the Service.
+    However, this does not apply to content created and uploaded by individuals or shared by third parties. The copyright of such contents belongs to the original creator of the contents.
 
-    7.Suspension of use of the service
-    The User may at any time stop using the Service and delete his/her account.
-    Suspension of account
-    The Service can suspend a user's account if the user is found to be in breach of the Terms of Use.
+    7.Suspension of Service Use
+    Users may stop using the Service at any time and delete their accounts and data.
+    7.1. Account Suspension
+    The Service may suspend a User's account if the Service confirms that the User has violated any of the Terms of Service.
 
-    8.Suspension of the provision of the Service
-    In the event of suspension or termination of the provision of the Service, the Service will notify the User of the suspension of the provision of the Service.`,
-    "button":{
+    8. Suspension of Service Provision
+    When the Service is to be suspended or terminated, the Service will notify the user of the suspension in advance.
+    However, this is not limited to special reasons.
+    `,
+    "button": {
         "accept": "Accept",
         "decline": "Decline"
     },
-    "termsMessage":{
-        "decline":"You have not agreed to the Terms of Use. If you do not agree to the Terms of Use, you may not use this service."
+    "termsMessage": {
+        "decline": "You have not agreed to the Terms of Use. If you do not agree to the Terms of Use, you may not use this service."
     }
 }
 
--- src/client/intl/en-us/others/en-us.terms.ts
+++ src/client/intl/en-us/others/en-us.terms.ts
@@ -2,39 +2,39 @@ const ENUSTermsTranslation = {
     "title": "Terms of Service",
     "content":`
     1. Acceptance of Terms of Use
-    By agreeing to the Terms of Use, you agree to use the Service. Your continued use of the Service constitutes your agreement to these Terms of Use.
+    By agreeing to the Terms of Use, you may use Feature Me (hereinafter referred to as "the Service"). By agreeing to the Terms of Use, you may use the Service.
     
     2.Changes to the Terms of Use
-    The Service may modify the Terms of Service from time to time as necessary. Modified Terms of Service shall become effective at the time of publication on the Service.
+    The Service may modify these Terms of Use from time to time as necessary. The amended Terms of Use shall become effective upon publication on the Service.
 
     3. Contents of the Service
     The Service provides the following services to users.
-    3.1. Playing music games 
-    3.2. Interaction with other companies using online functions
-    3.3. Creation and sharing of original contents
+    3.1. Provision of music game play
+    3.2. Interaction with other companies using online functions.
+    3.3. Creation and sharing of original contents.
 
     4. User's Responsibility
     Users shall not engage in any of the following acts when using the Service.
     4.1. Slandering, selling, lending, transferring, leasing, or contracting with others using the online functions of the Service.
     4.2. Any illegal acts against others using the Service.
-    4.3. Unauthorized diversion of the contents of the Service (whether commercial or non-commercial).
+    Unauthorized use of the Service (unauthorized modification programs, cheats, etc.).
+    4.4. Unauthorized diversion of the contents of the Service (whether commercial or non-commercial).
 
-    5. Complaints and consultation regarding the Service
-    Please use the in-game report function for complaints, consultations, and reports regarding the Service.
+    5. Additional information about the Service
+    If you have any complaints, concerns, or reports regarding the Service, please refer to the in-game report function or the project page on GitHub or Scrapbox.
 
-    6.Copyrights related to the Service
-    Copyrights to the Service belong to the copyright holders of the Service for all contents related to the Service.
-    6.1. Games in general: Mksk
-    6.2. Original music of the Service: Raetan, Goners
-    6.3. Original images of the Service: Mksk,Raetan,Goners
+    6.Copyrights for the Service
+    Copyrights to the Service for all officially distributed contents belong to the copyright holders of the Service.
+    However, this does not apply to content created and uploaded by individuals or shared by third parties. The copyright of such contents belongs to the original creator of the contents.
 
     7.Suspension of Service Use
-    Users may stop using the Service or delete their accounts at any time.
+    Users may stop using the Service at any time and delete their accounts and data.
     7.1. Account Suspension
-    The Service reserves the right to suspend a user's account if the user is found to be in violation of the Terms of Service.
+    The Service may suspend a User's account if the Service confirms that the User has violated any of the Terms of Service.
 
     8. Suspension of Service Provision
-    When the Service is to be suspended or terminated, the Service will notify the user of the suspension.
+    When the Service is to be suspended or terminated, the Service will notify the user of the suspension in advance.
+    However, this is not limited to special reasons.
     `,
     "button":{
         "accept": "Accept",
