3fae8e7b04947d53f62940e52cce0204e47e7fac
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-11 12:41:53 +0900

    add : setup settings page

---
+++ src/client/Pages/setup/contents/settings/settings.tsx
@@ -0,0 +1,44 @@
+import { useBeforeLeave, useNavigate } from "@solidjs/router";
+import GradientButton from "Components/Button/gradientButton/gradientButton";
+import TranslateText from "Components/TranslateText/TranslateText";
+import * as solid from "solid-js";
+import sleep from "Utils/sleep/sleep";
+
+import style from "./settings.module.scss"
+
+
+const SetupSettings: solid.Component = () => {
+    const navigate = useNavigate();
+    const [fadeOut, setFadeOut] = solid.createSignal<boolean>(false);
+
+    function navigation() {
+        navigate("/title");
+    }
+
+    useBeforeLeave(async (e) => {
+        console.log(location);
+        if (location.pathname != "/setup/settings") return;
+        if (!e.defaultPrevented) e.preventDefault();
+        setFadeOut(true);
+        await sleep(300);
+        e.retry(true);
+    })
+
+    return (
+        <div class={style.settings} classList={{ blackOut: fadeOut() }}>
+            <h1 class="shadowTitle"><TranslateText content="setup.settings" /></h1>
+            <hr />
+            <div class={style.contents}>
+
+            </div>
+            <hr />
+            <div class={style.interactions}>
+                <GradientButton onClick={navigation} class={style.button}>
+                    <TranslateText content="setup.next" />
+                </GradientButton>
+            </div>
+        </div>
+    )
+}
+
+export default SetupSettings;
