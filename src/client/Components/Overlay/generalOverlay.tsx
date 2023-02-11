195c957b888a168b9b6ed7ed42238f228d15f35e
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-11 17:35:36 +0900

    update : overlay

--- src/client/Components/Overlay/generalOverlay.tsx
+++ src/client/Components/Overlay/generalOverlay.tsx
@@ -3,15 +3,18 @@ import { offlineMode } from "State/network/offlineMode";
 
 import style from "./generalOverlay.module.scss";
 const GeneralOverlay: solid.Component = () => {
+    const [showOverlay, setShowOverlay] = solid.createSignal(true);
 
     solid.createEffect(() => {
-        console.log(offlineMode());
     })
 
     return (
         <div class={style.generalOverlay}>
-            <p>{offlineMode() && "Offline Mode"}</p>
-
+            <solid.Show when={showOverlay()} ><p>Feature Me Debug Overlay (Press F1 to toggle)</p></solid.Show>
+            <solid.Show when={offlineMode()}><p>Offline Mode</p></solid.Show>
+            <solid.Show when={showOverlay()}>
+                <p>{process.env.NODE_ENV == "production" ? "Production Build" : "Development Build"}</p>
+            </solid.Show>
         </div>
     )
 }
