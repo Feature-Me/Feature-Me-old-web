96c3a8363aa81a5462cdae7cd0d1976d551753aa
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-13 00:23:04 +0900

    update : settings modal

--- src/client/Pages/title/settingsModal.tsx
+++ src/client/Pages/title/settingsModal.tsx
@@ -3,10 +3,14 @@ import ModernModal from "Components/Modal/ModernModal/ModernModal";
 import TranslateText from "Components/TranslateText/TranslateText";
 import { useI18n } from "intl/intlContext";
 import *  as solid from "solid-js";
-import { showModal, setShowModal } from "./titleState";
+import { showModal, setShowModal, showDeleteSettingsModal, setShowDeleteSettingsModal, setCanBegin } from "./titleState";
 
 import style from "./title.module.scss";
 import SelectBox from "Components/Selectbox/selectbox";
+import GradientButton from "Components/Button/gradientButton/gradientButton";
+import clearStorage from "Utils/Storage/clear/clearStorage";
+import deleteDatabase from "Utils/Storage/database/deleteDatabase";
+import { deleteLocalStorage } from "Utils/Storage/LocalStorage/deleteLocalStorage";
 
 const SettingsModal: solid.Component = (props) => {
 
@@ -18,6 +22,11 @@ const SettingsModal: solid.Component = (props) => {
         { label: "Language : EN(US) - English(United States)", value: "en_us" },
     ]
 
+    function deleteData(func: Function) {
+        func();
+        setCanBegin(false);
+    }
+
     return (
         <ModernModal
             title={t("title.settings.title").toString()}
@@ -27,9 +36,20 @@ const SettingsModal: solid.Component = (props) => {
             <div class={style.settingsWindowInner}>
                 <SelectBox class={style.lngSettings} contents={languages} value={languages[0]} />
                 <LargeButton><TranslateText content="title.settings.export" /></LargeButton>
-                <LargeButton><TranslateText content="title.settings.clear" /></LargeButton>
+                <LargeButton onClick={() => setShowDeleteSettingsModal(true)}><TranslateText content="title.settings.clear" /></LargeButton>
                 <LargeButton><TranslateText content="title.terms" /></LargeButton>
             </div>
+
+            <ModernModal
+                title={t("title.settings.deleteDataModal.title").toString()}
+                show={showDeleteSettingsModal()}
+                onClickBackground={() => setShowDeleteSettingsModal(false)}
+                interactions={[{ label: t("title.settings.deleteDataModal.cancel").toString(), onClick: () => setShowDeleteSettingsModal(false) }]} >
+                <TranslateText content="title.settings.deleteDataModal.description" />
+                <GradientButton onClick={() => deleteData(deleteLocalStorage)}><TranslateText content="title.settings.deleteDataModal.saveData" /></GradientButton>
+                <GradientButton onClick={() => deleteData(deleteDatabase)}><TranslateText content="title.settings.deleteDataModal.resources" /></GradientButton>
+                <GradientButton onClick={() => deleteData(clearStorage)}><TranslateText content="title.settings.deleteDataModal.all" /></GradientButton>
+            </ModernModal>
         </ModernModal>
     )
 }
