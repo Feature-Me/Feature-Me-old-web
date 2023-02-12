0113ecd1abd6a015f5e9a9c680a8e9a13cf7e75f
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-12 23:57:35 +0900

    update : title modal state

--- src/client/Pages/title/titleState.tsx
+++ src/client/Pages/title/titleState.tsx
@@ -1,3 +1,5 @@
 import { createSignal } from "solid-js";
 
 export const [showModal, setShowModal] = createSignal(false);
+
+export const [showDeleteSettingsModal, setShowDeleteSettingsModal] = createSignal(false);
