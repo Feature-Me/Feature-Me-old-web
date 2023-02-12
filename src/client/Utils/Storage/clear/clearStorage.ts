e0b15eed50d1b8b909e3df92c356adaf01cf48e1
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-12 23:57:45 +0900

    update : clear storage util

--- src/client/Utils/Storage/clear/clearStorage.ts
+++ src/client/Utils/Storage/clear/clearStorage.ts
@@ -1,5 +1,9 @@
-function clearStorage(){
-    
+import deleteDatabase from "../database/deleteDatabase";
+import { deleteLocalStorage } from "../LocalStorage/deleteLocalStorage";
+
+function clearStorage() {
+    deleteLocalStorage();
+    deleteDatabase();
 }
 
 export default clearStorage;
