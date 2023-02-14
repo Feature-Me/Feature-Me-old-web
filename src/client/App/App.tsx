c9a4d2ac5f8cd9a7f51c311a00955b3413a75aab
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-14 22:52:59 +0900

    update : entry point

--- src/client/App/App.tsx
+++ src/client/App/App.tsx
@@ -1,12 +1,14 @@
-import i18n from "intl/intl";
 import I18nProvider from "Components/intlProvider/intlProvider";
 import i18next from "i18next";
 import * as solid from "solid-js";
 import { Router, staticIntegration } from "@solidjs/router";
 import toast, { Toaster } from "solid-toast";
+import { TransProvider, useTransContext } from "@mbarzda/solid-i18next";
 
 import PageRouter from "Router/router";
 
+import { intlConfig } from "intl/intl";
+import LanguageHelper from "Components/intlProvider/languageHelper";
 import Loading from "Components/LoadingCover/loading";
 import CrashHandler from "Components/ErrorBoundary/ApplicatonCrashHandler/crashHandler";
 import Background from "Components/Background/background";
@@ -14,7 +16,7 @@ import GeneralOverlay from "Components/Overlay/generalOverlay";
 
 import style from "./App.module.scss";
 import "./style.scss";
-
+import i18n from "intl/intl";
 
 const App: solid.Component = () => {
     const [loaded, setLoaded] = solid.createSignal(true);
@@ -23,20 +25,10 @@ const App: solid.Component = () => {
         setLoaded(true);
     });
 
-    solid.createEffect(() => {
-        if (!loaded()) return;
-        console.log(`Language : ${i18next.language}`);
-        console.log(`All avaliable languages : ${i18next.languages}`);
-
-        const environment = JSON.parse(localStorage.getItem("environment") || "{}");
-        environment.language = i18next.language;
-        localStorage.setItem("environment", JSON.stringify(environment));
-    });
-
     return (
         <solid.ErrorBoundary fallback={(err, reset) => <CrashHandler error={err} reset={reset} />}>
             <solid.Show when={loaded()} fallback={<Loading />}>
-                <I18nProvider i18n={i18next}>
+                <TransProvider options={intlConfig}>
                     {/* memory router */}
                     <Router source={staticIntegration({ value: "" })}>
                         <div class={style.app} onContextMenu={(e) => e.preventDefault()}>
@@ -52,7 +44,8 @@ const App: solid.Component = () => {
                             </div>
                         </div>
                     </Router>
-                </I18nProvider>
+                    <LanguageHelper />
+                </TransProvider>
             </solid.Show>
         </solid.ErrorBoundary>
     );
