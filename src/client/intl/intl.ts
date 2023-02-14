5b976996aa99597b5bd2e6ef6666436f645a4297
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-14 22:52:17 +0900

    update : intl init

--- src/client/intl/intl.ts
+++ src/client/intl/intl.ts
@@ -1,17 +1,15 @@
-import i18next from 'i18next';
-
 import en_us from "./en-us/en-us";
 import en_uk from "./en-uk/en-uk";
 import ja from "./ja/ja"
 import zh_cn from "./zh-cn/zh-cn";
-
+import i18next from 'i18next';
 
 
 const language: string = (localStorage.getItem("environment") && JSON.parse(localStorage.getItem("environment")!).language) || navigator.language.toLowerCase().replace(/-/g, "_");
 
-const i18n = i18next.init({
+const intlConfig = {
     lng: language,
-    fallbackLng: 'ja',
+    fallbackLng: ['ja', "en_us", "en_uk"],
     resources: {
         en_us: { translation: en_us },
         en_uk: { translation: en_uk },
@@ -21,6 +19,9 @@ const i18n = i18next.init({
         escapeValue: false,
     }
 
-});
+};
+
+const i18n = i18next.init(intlConfig);
 
 export default i18n;
+export { intlConfig };
