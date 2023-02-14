17bb5315fa482dff94e33a8cd1ef5d05e61a1a7c
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-14 22:52:35 +0900

    add : language helper

---
+++ src/client/Components/intlProvider/languageHelper.tsx
@@ -0,0 +1,28 @@
+import { useTransContext } from "@mbarzda/solid-i18next";
+import i18next from "i18next";
+import * as solid from "solid-js";
+
+const LanguageHelper: solid.Component = () => {
+
+    const [t, intl] = useTransContext();
+
+    function changeLanguage() {
+        console.log(`Language : ${intl.getI18next().language}`);
+        console.log(`All avaliable languages : ${intl.getI18next().languages}`);
+
+        const environment = JSON.parse(localStorage.getItem("environment") || "{}");
+        environment.language = intl.getI18next().language;
+        localStorage.setItem("environment", JSON.stringify(environment));
+    }
+
+    solid.onMount(() => {
+        intl.getI18next().on("languageChanged", changeLanguage);
+    });
+    solid.onCleanup(() => {
+        intl.getI18next().off("languageChanged", changeLanguage);
+    });
+
+    return (<></>)
+}
+
+export default LanguageHelper;
