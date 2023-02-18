c142a15c0f1169f9a2412bb676d10eb7ba8d430b
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-18 17:11:33 +0900

    update : webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require("path");
 const TsConfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
 module.exports = {
-    entry: path.resolve(__dirname, "src/client","App", "index.tsx"),
+    entry: path.resolve(__dirname, "src/client", "App", "index.tsx"),
     mode: "development",
     output: {
         path: path.resolve(__dirname, "dist/scripts"),
@@ -11,7 +11,7 @@ module.exports = {
     resolve: {
         extensions: ['.ts', '.tsx', '.js', 'jsx'],
         plugins: [
-            new TsConfigPathsPlugin({ configFile: path.join(__dirname, "src/client","tsconfig.json") }),
+            new TsConfigPathsPlugin({ configFile: path.join(__dirname, "src/client", "tsconfig.json") }),
         ]
     },
     target: "web",
@@ -55,6 +55,7 @@ module.exports = {
             { test: /\.(ttf|TTF|otf|OTF)$/, type: "asset/inline" },
             { test: /\.svg$/, type: "asset/inline" },
             { test: /\.mp3$/, type: "asset/inline" },
+            { test: /\.mp4|\.m4a$/, type: "asset/inline" },
         ]
     }
 }
