5529ea96bc4d9d2a2aa2b5367e129a64f77a4b8c
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-18 17:16:38 +0900

    update :  button components

--- src/client/Components/Button/gradientButton/gradientButton.tsx
+++ src/client/Components/Button/gradientButton/gradientButton.tsx
@@ -1,6 +1,10 @@
 import * as solid from "solid-js";
+import playAudio from "Utils/PlayAudio/playAudio";
 
 import style from "./gradientButton.module.scss";
+
+import clickSound from "Assets/Sounds/uiFallBack/clickDown.m4a";
+import selectSound from "Assets/Sounds/uiFallBack/select.m4a";
 
 interface buttonPropsType extends solid.JSX.ButtonHTMLAttributes<HTMLButtonElement> {
     accentColor?: string
@@ -11,19 +15,34 @@ const GradientButton: solid.Component<buttonPropsType> = (props) => {
 
     let buttonRef: HTMLButtonElement | undefined;
 
-    //if there is custom accent color,set lisner to override color
-    solid.createEffect(() => {
+    solid.onMount(() => {
+        if (!buttonRef) return;
+        buttonRef.addEventListener("click", handleClick);
+        buttonRef.addEventListener("pointerenter", handleHover);
+        buttonRef.addEventListener("focusin", handleHover);
     })
 
-    function vibration() {
+    solid.onCleanup(() => {
+        if (!buttonRef) return
+        buttonRef.removeEventListener("click", handleClick);
+        buttonRef.removeEventListener("pointerenter", handleHover);
+        buttonRef.removeEventListener("focusin", handleHover);
+    })
+
+    function handleClick() {
+        playAudio(clickSound);
         navigator.vibrate(50);
+    }
+
+    function handleHover() {
+        playAudio(selectSound);
     }
 
     const buttonProps = props;
     delete buttonProps.accentColor;
 
     return (
-        <button tabIndex={0} ref={buttonRef} onClick={vibration} {...buttonProps} class={`${style.gradientButton} ${props.class || ""}`} >
+        <button tabIndex={0} ref={buttonRef} {...buttonProps} class={`${style.gradientButton} ${props.class || ""}`} >
             {props.children}
         </button>
     )
--- src/client/Components/Button/largeButton/largeButton.tsx
+++ src/client/Components/Button/largeButton/largeButton.tsx
@@ -1,6 +1,10 @@
 import * as solid from "solid-js";
+import playAudio from "Utils/PlayAudio/playAudio";
 
 import style from "./largeButton.module.scss";
+
+import clickSound from "Assets/Sounds/uiFallBack/clickDown.m4a";
+import selectSound from "Assets/Sounds/uiFallBack/select.m4a";
 
 
 interface buttonPropsType extends solid.JSX.ButtonHTMLAttributes<HTMLButtonElement> {
@@ -14,12 +18,31 @@ const LargeButton: solid.Component<buttonPropsType> = (props) => {
     const buttonProps = props;
     delete buttonProps.accentColor;
 
-    function vibration() {
+    solid.onMount(() => {
+        if (!buttonRef) return;
+        buttonRef.addEventListener("click", handleClick);
+        buttonRef.addEventListener("pointerenter", handleHover);
+        buttonRef.addEventListener("focusin", handleHover);
+    })
+
+    solid.onCleanup(() => {
+        if (!buttonRef) return
+        buttonRef.removeEventListener("click", handleClick);
+        buttonRef.removeEventListener("pointerenter", handleHover);
+        buttonRef.removeEventListener("focusin", handleHover);
+    })
+
+    function handleClick() {
+        playAudio(clickSound);
         navigator.vibrate(50);
     }
 
+    function handleHover() {
+        playAudio(selectSound);
+    }
+
     return (
-        <button tabIndex={0} ref={buttonRef} onClick={vibration} {...buttonProps} class={`${style.largeButton} ${props.class || ""}`}>
+        <button tabIndex={0} ref={buttonRef} {...buttonProps} class={`${style.largeButton} ${props.class || ""}`}>
             <h2>{props.children}</h2>
         </button>
     )
