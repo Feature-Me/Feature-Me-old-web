76f2866b75abc2adad289ba4f7e8b56c6b400b1a
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-18 17:15:12 +0900

    add : home page

---
+++ src/client/Pages/home/home.tsx
@@ -0,0 +1,48 @@
+import { useBeforeLeave, useLocation, useNavigate } from "@solidjs/router";
+import * as solid from "solid-js";
+import { BsArrowUp, BsArrowUpLeft, BsGear } from "solid-icons/bs";
+import { useTransContext } from "@mbarzda/solid-i18next";
+
+import TranslateText from "Components/TranslateText/TranslateText";
+
+import playAudio from "Utils/PlayAudio/playAudio";
+
+import style from "./home.module.scss";
+
+import clickSound from "Assets/Sounds/uiFallBack/clickDown.m4a";
+import selectSound from "Assets/Sounds/uiFallBack/select.m4a";
+import Header from "../../Components/Header/header";
+
+
+const Home: solid.Component = () => {
+    const navigate = useNavigate();
+    const [t, intl] = useTransContext();
+    const [fadeOut, setFadeOut] = solid.createSignal<boolean>(false);
+
+    let containerRef: HTMLDivElement | undefined;
+
+    useBeforeLeave(async (e) => {
+        if (!e.defaultPrevented) e.preventDefault();
+        if (!containerRef) return;
+        setFadeOut(true);
+        setTimeout(() => {
+            e.retry(true);
+        }, 1000)
+    })
+
+    return (
+        <div class={style.home} ref={containerRef} classList={{ blackOut: fadeOut() }}>
+            <Header />
+            <main class={style.main}>
+                <div class={style.news}>News</div>
+                <div class={style.modeList}>
+                    <div class={style.listContent}>
+                        Mode Name
+                    </div>
+                </div>
+            </main>
+        </div >
+    )
+}
+
+export default Home;
