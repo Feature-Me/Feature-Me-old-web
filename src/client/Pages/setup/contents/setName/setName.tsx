7c15835e004722fc24907887797b16ed4d70320c
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-19 23:19:01 +0900

    update : setname in setup

--- src/client/Pages/setup/contents/setName/setName.tsx
+++ src/client/Pages/setup/contents/setName/setName.tsx
@@ -1,14 +1,21 @@
 import * as solid from "solid-js";
 
-import style from "./setName.module.scss"
 import TranslateText from "Components/TranslateText/translateText";
 import GradientButton from "Components/Button/gradientButton/gradientButton";
 import { useNavigate } from "@solidjs/router";
 import { useTransContext } from "@mbarzda/solid-i18next";
 
+import NormalInput from "Components/TextInput/NormalInput/NormalInput";
+
+import style from "./setName.module.scss"
+
 const SetupSetName: solid.Component = () => {
     const navigate = useNavigate();
     const [t, intl] = useTransContext();
+
+    let inputRef: HTMLInputElement | undefined;
+
+    const defaultName = JSON.parse(localStorage.getItem("userData") || '{"userInfo":{"name":""}}').userInfo.name
 
     const imports = [
         { label: "Feature Me Alpha (.fmcfg)", func: () => { } },
@@ -21,13 +28,22 @@ const SetupSetName: solid.Component = () => {
         navigate("../settings");
     }
 
+    function setName() {
+        if (!inputRef) return;
+        let name = inputRef.value;
+        const userData = JSON.parse(localStorage.getItem("userData") || "{initializedSettings:false}");
+        userData.userInfo.name = name;
+        localStorage.setItem("userData", JSON.stringify(userData));
+        navigation();
+    }
+
     return (
         <div class={style.setName} >
             <h1 class="shadowTitle"><TranslateText key="setup.setName.title" /></h1>
             <div class={style.content}>
-                <p><TranslateText key="setup.setName.description" /></p><br />
-
-                <GradientButton><TranslateText key="setup.next" /></GradientButton>
+                <p><TranslateText key="setup.setName.description" /></p>
+                <NormalInput ref={inputRef} value={defaultName} maxLength={15} />
+                <GradientButton onClick={setName}><TranslateText key="setup.next" /></GradientButton>
             </div>
         </div>
     )
