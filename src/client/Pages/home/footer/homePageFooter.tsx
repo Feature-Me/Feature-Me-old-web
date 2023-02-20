d9a329a365a43811df4e19d41ff37d1803419d96
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-20 05:14:51 +0000

    add : homepage footer

---
+++ src/client/Pages/home/footer/homePageFooter.tsx
@@ -0,0 +1,14 @@
+import * as solid from "solid-js";
+
+import style from "./homePageFooter.module.scss";
+
+
+const HomePageFooter: solid.Component = () => {
+    return(
+        <footer class={style.footer}>
+
+        </footer>
+    )
+}
+
+export default HomePageFooter;
