bfa556b883318854941da00b471ad490c065c56c
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-02-20 21:49:46 +0900

    remove : app initializer

--- src/client/Router/router.tsx
+++ src/client/Router/router.tsx
@@ -1,7 +1,6 @@
 import * as solid from "solid-js";
 import { Routes, Route, Navigate } from "@solidjs/router";
 
-import Initializer from "Pages/appInitializer/appInitializer";
 import SplashScreen from "Pages/splash/splashScreen";
 import Loader from "Pages/appLoader/loader";
 import Title from "Pages/title/title";
