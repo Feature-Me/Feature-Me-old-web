163ef79174ed31500e82cea50248345e50c80706
Makisaka <81460622+Mkskdesu@users.noreply.github.com>
2023-10-06 17:32:30 +0900

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,19 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import devtools from 'solid-devtools/vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-    plugins: [solid()],
+    plugins: [
+        solid(),
+        tsconfigPaths(),
+        devtools({
+            autoname: true,
+            locator: {
+                targetIDE: 'vscode',
+                componentLocation: true,
+                jsxLocation: true,
+            }
+        })
+    ],
 })
